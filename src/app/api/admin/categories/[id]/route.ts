144a649c764da307dc28ba8f47463ca06ef8b8e1
kana <k.salon.du.l@gmail.com>
2024-09-10 14:13:29 +0900

    fix

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -5,22 +5,37 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export const GET = async (
-  request: NextRequest,
+  _request: NextRequest,
   { params }: { params: { id: string } }
 ) => {
   const { id } = params;
 
   try {
     const category = await prisma.category.findUnique({
-      where: {
-        id: parseInt(id),
-      },
+      where: { id: parseInt(id) },
     });
+
+    if (!category) {
+      return NextResponse.json(
+        { status: "Category not found" },
+        { status: 404 }
+      );
+    }
 
     return NextResponse.json({ status: "OK", category }, { status: 200 });
   } catch (error) {
-    if (error instanceof Error)
-      return NextResponse.json({ status: error.message }, { status: 400 });
+    if (error instanceof Error) {
+      console.error("Error fetching category:", error.message);
+      return NextResponse.json({ status: error.message }, { status: 500 });
+    } else {
+      console.error("Unknown error:", error);
+      return NextResponse.json(
+        { status: "Unknown error occurred" },
+        { status: 500 }
+      );
+    }
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
